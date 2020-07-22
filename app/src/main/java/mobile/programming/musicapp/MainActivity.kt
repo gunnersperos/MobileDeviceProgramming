1460dd8adc230f1e4e194becdadd18c39449e9c4
comynssj <67163817+comynssj@users.noreply.github.com>
2020-07-22 13:22:38 -0400

    Update MainActivity.kt

--- app/src/main/java/mobile/programming/musicapp/MainActivity.kt
+++ app/src/main/java/mobile/programming/musicapp/MainActivity.kt
@@ -10,3 +10,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+FirebaseInstanceId.getInstance().instanceId
+        .addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "getInstanceId failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new Instance ID token
+            val token = task.result?.token
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
