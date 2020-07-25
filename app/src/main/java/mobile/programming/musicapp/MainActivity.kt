e70d2dd933171e6c95e5bc2856c2acc7211b2d17
stoberdl <stoberdl@mail.uc.edu>
2020-07-24 22:26:24 -0400

    Moved the incorrect implementations to correct build.gradle Corrected the service call in AndroidManifest.xml Moved the messaging service from the root directory to the services folder Corrected the code location, added imports, and fixed the added code in MainActivity.kt

--- app/src/main/java/mobile/programming/musicapp/MainActivity.kt
+++ app/src/main/java/mobile/programming/musicapp/MainActivity.kt
@@ -6,10 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.messaging.Constants.MessagePayloadKeys.SENDER_ID
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.RemoteMessage
-import mobile.programming.musicapp.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
