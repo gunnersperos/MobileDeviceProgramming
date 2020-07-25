0f844405bc86623ce36a3a2ed7ac8621572cff0a
stoberdl <stoberdl@mail.uc.edu>
2020-07-24 22:14:23 -0400

    Moved the incorrect implementations to correct build.gradle Corrected the service call in AndroidManifest.xml Moved the messaging service from the root directory to the services folder Corrected the code location, added imports, and fixed the added code in MainActivity.kt

---
+++ app/src/main/java/mobile/programming/musicapp/service/MyFirebaseMessagingService.kt
@@ -0,0 +1,26 @@
+package mobile.programming.musicapp.service
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.i("FirebaseService ", "Refreshed token :: $token")
+
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String) {
+
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.i("FirebaseService ", "Message :: $message")
+    }
+}
