17727ae8d1cbafac217cf0de1233c1a2ea895bcb
bet45140 <35737649+bet45140@users.noreply.github.com>
2020-07-25 21:16:50 -0400

    Update MainActivity.kt
    Since this file was causing a conflict with the merge, I copied what you had in the Master branch version of this file into this file.

--- app/src/main/java/mobile/programming/musicApp/MainActivity.kt
+++ app/src/main/java/mobile/programming/musicApp/MainActivity.kt
@@ -2,36 +2,29 @@ package mobile.programming.musicApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import mobile.programming.musicapp.R
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                Log.d("hi", token.toString())
+                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+            })
     }
 }
-
-FirebaseInstanceId.getInstance().instanceId
-        .addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "getInstanceId failed", task.exception)
-                return@OnCompleteListener
-            }
-
-            // Get new Instance ID token
-            val token = task.result?.token
-
-            // Log and toast
-            val msg = getString(R.string.msg_token_fmt, token)
-            Log.d(TAG, msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        })
-
-val fm = FirebaseMessaging.getInstance()
-fm.send(RemoteMessage.Builder("$SENDER_ID@fcm.googleapis.com")
-        .setMessageId(Integer.toString(messageId))
-        .addData("my_message", "Hello World")
-        .addData("my_action", "SAY_HELLO")
-        .build())
-
-FirebaseMessaging.getInstance().isAutoInitEnabled = true
