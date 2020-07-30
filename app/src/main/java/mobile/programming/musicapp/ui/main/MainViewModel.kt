7333a35f167e3545712b8fcc53797cce0335385a
stoberdl <stoberdl@mail.uc.edu>
2020-07-29 23:12:00 -0400

    Add price var to the json and instruement live data

--- app/src/main/java/mobile/programming/musicapp/ui/main/MainViewModel.kt
+++ app/src/main/java/mobile/programming/musicapp/ui/main/MainViewModel.kt
@@ -2,6 +2,8 @@ package mobile.programming.musicapp.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import mobile.programming.musicapp.dto.Instrument
 import mobile.programming.musicapp.service.InstrumentService
 
@@ -10,8 +12,19 @@ class MainViewModel: ViewModel() {
     var instruments: MutableLiveData<ArrayList<Instrument>> = MutableLiveData<ArrayList<Instrument>>()
     var instrumentService: InstrumentService =
         InstrumentService()
+    private lateinit var  firestore: FirebaseFirestore
+
+    init{
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
 
     internal fun getAllInstruments(){
         instruments = instrumentService.fetchInstruments()
+
+    }
+
+    fun saveToFirestore(arrayList: ArrayList<String>){
+        firestore.collection("users")
     }
 }
