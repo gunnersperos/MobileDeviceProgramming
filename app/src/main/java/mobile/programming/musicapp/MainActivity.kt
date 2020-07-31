642cdbc42a476c4314e558bec4ff654824210618
stoberdl <stoberdl@mail.uc.edu>
2020-07-31 17:36:08 -0400

    Add price var to the json and instruement live data

--- app/src/main/java/mobile/programming/musicapp/MainActivity.kt
+++ app/src/main/java/mobile/programming/musicapp/MainActivity.kt
@@ -1,17 +1,20 @@
 package mobile.programming.musicapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
+import mobile.programming.musicapp.ui.main.MainViewModel
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lateinit var mvm: MainViewModel
+        lateinit var userID: String
 
         FirebaseInstanceId.getInstance().instanceId
             .addOnCompleteListener(OnCompleteListener { task ->
@@ -25,7 +28,24 @@ class MainActivity : AppCompatActivity() {
 
                 Log.d("token", token.toString())
                 Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                userID = token.toString()
+
+                //sample code for saving to firestore DB
+                mvm = MainViewModel()
+                val list = arrayListOf("piano", "guitar", "drums")
+                mvm.saveToFirestore(list, userID)
+                mvm.LoadFromFirestore(userID)
+                //TODO update main UI buttons to reveal unlocked instruments on load
+
+
+
             })
+
+
+
+
+
+
     }
 }
 
--- app/src/main/java/mobile/programming/musicapp/ui/main/MainViewModel.kt
+++ app/src/main/java/mobile/programming/musicapp/ui/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package mobile.programming.musicapp.ui.main
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
@@ -12,7 +13,7 @@ class MainViewModel: ViewModel() {
     var instruments: MutableLiveData<ArrayList<Instrument>> = MutableLiveData<ArrayList<Instrument>>()
     var instrumentService: InstrumentService =
         InstrumentService()
-    private lateinit var  firestore: FirebaseFirestore
+    private var firestore: FirebaseFirestore
 
     init{
         firestore = FirebaseFirestore.getInstance()
@@ -24,7 +25,32 @@ class MainViewModel: ViewModel() {
 
     }
 
-    fun saveToFirestore(arrayList: ArrayList<String>){
-        firestore.collection("users")
+    fun saveToFirestore(unlockedInstrumentsList: ArrayList<String>,userID: String){
+
+        val userData = hashMapOf(
+            "unlockedInstruments" to unlockedInstrumentsList
+        )
+
+        firestore.collection("users").document(userID)
+            .set(userData)
+            .addOnSuccessListener { Log.d("firestore", "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w("firestore", "Error writing document", e) }
+
+    }
+
+    fun LoadFromFirestore(userID: String){
+        val instrumentRef = firestore.collection("users").document(userID)
+        instrumentRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("firestore load", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("firestore load", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("firestore load", "get failed with ", exception)
+            }
+
     }
 }
