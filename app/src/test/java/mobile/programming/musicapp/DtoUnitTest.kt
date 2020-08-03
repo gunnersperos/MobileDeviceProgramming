1b6a9b34981d6484e020a271c6e9e11ad8422e2f
stoberdl <stoberdl@mail.uc.edu>
2020-08-02 20:53:35 -0400

    Added a unit test Cleaned up code

--- app/src/test/java/mobile/programming/musicapp/DtoUnitTest.kt
+++ app/src/test/java/mobile/programming/musicapp/DtoUnitTest.kt
@@ -3,7 +3,6 @@ package mobile.programming.musicapp
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import mobile.programming.musicapp.dto.Instrument
 import mobile.programming.musicapp.dto.UserData
-import mobile.programming.musicapp.ui.main.TestViewModel
 import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
@@ -14,15 +13,10 @@ class DtoUnitTest {
     //Nothing to unit test besides instrument data integration and the data classes in the current project state. Firebase doesn't allow unit testing.
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
-    lateinit var mvm: TestViewModel
+
 
     @Test
     internal fun testDataClasses() {
-        mvm = TestViewModel()
-        createMockData()
-    }
-
-    private fun createMockData() {
         val testInstrument = Instrument( 0,"Clarinet", "Percussion", 100)
         //test class function
         testInstrument.toString()
@@ -36,4 +30,5 @@ class DtoUnitTest {
         assertNotNull(testInstrument)
         assertNotNull(userData)
     }
+    
 }
